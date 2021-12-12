5088acfaf8613251312e9400e52d4068e0026557
flaxalf <flavio.scaccia@gmail.com>
2021-12-13 00:16:04 +0100

    Level one activity

---
+++ app/src/main/java/it/sapienza/solveit/ui/levels/LevelOneActivity.kt
@@ -0,0 +1,12 @@
+package it.sapienza.solveit.ui.levels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.sapienza.solveit.R
+
+class LevelOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_one)
+    }
+}
