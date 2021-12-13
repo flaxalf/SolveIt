90032bc91b8f9609ccd98c07e7c92ae5494c668b
flaxalf <flavio.scaccia@gmail.com>
2021-12-13 15:34:54 +0100

    levels in fragment structure

--- app/src/main/java/it/sapienza/solveit/ui/levels/LevelOneActivity.kt
+++ app/src/main/java/it/sapienza/solveit/ui/levels/LevelOneActivity.kt
@@ -2,11 +2,29 @@ package it.sapienza.solveit.ui.levels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import it.sapienza.solveit.R
 
 class LevelOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level_one)
+
+        val btn = findViewById(R.id.btnNextFragment) as Button
+
+        /* Do this for 5 levels and replace them */
+        val levelOneFragment = LevelOneFragment()
+        val levelTwoFragment = LevelTwoFragment()
+
+        //val levelOneFragment = LevelOneFragment()
+        //val levelOneFragment = LevelOneFragment()
+        //val levelOneFragment = LevelOneFragment()
+
+        btn.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, levelTwoFragment)
+                commit()
+            }
+        }
     }
 }
