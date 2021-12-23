8067e17de7d7101cfdd3bc4deeef05ed795f9ab4
flaxalf <flavio.scaccia@gmail.com>
2021-12-23 23:02:04 +0100

    cleaning

--- app/src/main/java/it/sapienza/solveit/ui/levels/CustomDialogFragment.kt
+++ app/src/main/java/it/sapienza/solveit/ui/levels/CustomDialogFragment.kt
@@ -6,24 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import it.sapienza.solveit.R
 
 class CustomDialogFragment: DialogFragment() {
-    lateinit var nextFragmentButton: Button
+    private lateinit var nextFragmentButton: Button
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val bundle: Bundle? = this.arguments
         var info_level : Int = 0
 
         if (bundle != null) {
             info_level = bundle.getInt("Level")
-            Log.d("levelz", "level is: "+info_level)
         }
 
-        var rootView: View = inflater.inflate(R.layout.fragment_custom_dialog, container, false)
+        val rootView: View = inflater.inflate(R.layout.fragment_custom_dialog, container, false)
 
         nextFragmentButton = rootView.findViewById(R.id.nextLevelButton) as Button
         nextFragmentButton.setOnClickListener {
