6216a1f53e4b4219cfb3bcfb1a20f590e16771da
Marco <condiviso.mobile@gmail.com>
2022-01-14 23:32:16 +0100

    level two multi cloud impl

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,12 @@
 from flask import Flask,jsonify
 from flask import request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 import string
 import random
 import time
 
 app = Flask(__name__)
 api = Api(app)
-
-
-parser = reqparse.RequestParser()
-
-lat=0
-lon=0
 
 
 #map that contains key = match id and value = a matchInstance
@@ -24,23 +18,6 @@ class matchInstance:
         self.timestamp = time.time()
         self.player1 = p1
         self.player2 = p2
-
-
-#map that contains key = match id and value = a levelOne instance
-levelOneDict = {}
-
-class levelOneInstance:
-    def __init__(self, id, b1 = 0, b2 = 0):
-        self.id = id
-        self.button1 = b1
-        self.button2 = b2
-        self.right1 = False
-        self.right2 = False
-
-    def isLevelPassed(self):
-        if self.right1 == True and self.right2 == True:
-            return True
-        return False
 
 
 class matchingMulti(Resource):
@@ -73,6 +50,24 @@ class matchingMulti(Resource):
 api.add_resource(matchingMulti,'/matchingMulti')
 
 
+
+#map that contains key = match id and value = a levelOne instance
+levelOneDict = {}
+
+class levelOneInstance:
+    def __init__(self, id, b1 = 0, b2 = 0):
+        self.id = id
+        self.button1 = b1
+        self.button2 = b2
+        self.right1 = False
+        self.right2 = False
+
+    def isLevelPassed(self):
+        if self.right1 == True and self.right2 == True:
+            return True
+        return False
+
+
 class levelOneMulti(Resource):
     def get(self):
         id = request.args.get('id')
@@ -88,7 +83,9 @@ class levelOneMulti(Resource):
         else:
             reply_msg['button'] = levelInstance.button1
 
-        reply_msg['success'] = levelInstance.isLevelPassed()
+        passed = levelInstance.isLevelPassed()
+        reply_msg['success'] = passed
+
         return jsonify(reply_msg)
 
     def post(self):
@@ -112,14 +109,73 @@ class levelOneMulti(Resource):
 
 api.add_resource(levelOneMulti,'/levelOneMulti')
 
+
+#map that contains key = match id and value = a levelOne instance
+levelTwoDict = {}
+
+class levelTwoInstance:
+    def __init__(self, id, n1 = 0, n2 = 0):
+        self.id = id
+        self.number1 = n1
+        self.number2 = n2
+
+    def isLevelPassed(self):
+        if self.number1 == self.number2:
+            return True
+        return False
+
+
+
 class levelTwoMulti(Resource):
     def get(self):
         id = request.args.get('id')
+        user = request.args.get('user')
+
         reply_msg = {}
+        if levelTwoDict.get(id) is None:
+            levelTwoDict[id] = levelTwoInstance(id)
+
+        levelInstance = levelTwoDict[id]
+        if levelInstance.number1 != 0 and levelInstance.number2 != 0:
+            if user == matchDict[id].player1:
+                reply_msg['number'] = levelInstance.number2
+            else:
+                reply_msg['number'] = levelInstance.number1
+
+            passed = levelInstance.isLevelPassed()
+            reply_msg['success'] = passed
+
+        else:
+            reply_msg['success'] = 'wait'
+
         return jsonify(reply_msg)
 
     def post(self):
         id = request.args.get('id')
+        user = request.args.get('user')
+        number = request.args.get('number')
+
+        if levelTwoDict.get(id) is None:
+            levelTwoDict[id] = levelTwoInstance(id)
+
+        levelInstance = levelTwoDict[id]
+        #check if below we need locks, cause a call from a player modifies
+        #the value from the other player
+        if user == matchDict[id].player1:
+            if levelInstance.number1 != 0:
+                #clean the level instance here, if someone posts a new number
+                #after already posting one, it means that the level has
+                #restarted so the status of the instance must be restarted,
+                #so the player number must be set to the new one
+                #and the other player number must be set to 0 again
+                levelInstance.number2 = 0
+            levelInstance.number1 = number
+        else:
+            if levelInstance.number2 != 0:
+                #same as above
+                levelInstance.number1 = 0
+            levelInstance.number2 = number
+
         return jsonify("POST: ok")
 
 api.add_resource(levelTwoMulti,'/levelTwoMulti')
@@ -145,6 +201,7 @@ class endGameMulti(Resource):
         return jsonify(reply_msg)
 
     def post(self):
+        #TODO clean all the dicts here
         id = request.args.get('id')
         return jsonify("POST: ok")
 
@@ -161,30 +218,6 @@ class leaderboard(Resource):
 
 api.add_resource(leaderboard,'/leaderboard')
 
-
-
-class myHUB(Resource):
-
-    def get(self):
-        global lat,lon
-        reply_msg = {'lat':"-1",'lon':"-1"}
-        parser.remove_argument("lat")
-        parser.remove_argument("lon")
-        args = parser.parse_args()
-        reply_msg['lat']=str(lat)
-        reply_msg['lon']=str(lon)
-        return jsonify(reply_msg)
-
-    def post(self):
-        global lat,lon
-        parser.add_argument('lat',type=float,required=True)
-        parser.add_argument('lon',type=float,required=True)
-        args = parser.parse_args()
-        lat=args['lat']
-        lon=args['lon']
-        return jsonify("POST: ok")
-
-api.add_resource(myHUB,'/test')
 
 if __name__ == '__main__':
     print('starting SolveIt apis...waiting for players')
